e4576b647c83eb689f440357adb50584c722f98d
karart81 <karapart1981@gmail.com>
2024-08-25 11:49:38 +0400

    I generated password with pure JS

---
+++ main.js
@@ -0,0 +1,15 @@
+function generateRandomPassword(length) {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=';
+    let password = '';
+    
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      password += characters[randomIndex];
+    }
+  
+    return password;
+  }
+  
+  
+  const password = generateRandomPassword(12);
+  console.log(password);
